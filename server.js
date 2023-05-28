673140965d486092bab1dca884ca8e5bf7dc37b8
NR <89783021+deegle0-0@users.noreply.github.com>
2023-05-28 14:15:34 +1000

    Update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('hehe change');
+  res.send('NEW BIG CHANGES');
 });
 
 app.listen(PORT, HOST, () => {
